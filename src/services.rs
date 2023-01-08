54bc94cef3e355ffc2f83b137139f290d4137215
1nteger-c <pk2861@naver.com>
2023-01-08 18:21:26 +0900

    feat: Add code to delete user

--- src/services.rs
+++ src/services.rs
@@ -1,6 +1,6 @@
 use crate::AppState;
 use actix_web::{
-    patch, post,
+    post, patch, delete,
     web::{Data, Json},
     HttpResponse, Responder,
 };
@@ -22,7 +22,16 @@ pub struct PatchUserBody {
     pub name: String,
     pub password: String,
     pub new_name: String,
-    pub new_password: String,
+    pub new_password: String
+}
+#[derive(Deserialize)]
+pub struct DeleteUserBody {
+    pub name: String,
+    pub password: String,
+}
+#[derive(FromRow)]
+struct IdRow {
+    pub id: i32
 }
 
 #[derive(FromRow)]
@@ -100,4 +109,25 @@ pub async fn patch_user(state: Data<AppState>, body: Json<PatchUserBody>) -> imp
         Ok(user) => HttpResponse::Ok().json(user),
         Err(_) => HttpResponse::InternalServerError().json("Failed to patch user"),
     }
+}
+
+#[delete("/user")]
+pub async fn delete_user(state: Data<AppState>, body: Json<DeleteUserBody>) -> impl Responder{
+    let user_id: i32 = match user_auth(&state, body.name.to_string(), body.password.to_string())
+        .await
+        {
+            -1 => return HttpResponse::BadRequest().json("Authentication failed"),
+            x => x
+        };
+    match sqlx::query(
+        "DELETE FROM userInfo WHERE id = $1"
+    )
+        .bind(user_id)
+        .execute(&state.db)
+        .await
+    {
+        Ok(_) => HttpResponse::Ok().json("Successfully"),
+        Err(_) => HttpResponse::InternalServerError().json("Failed to delete user")
+    }
+
 }
